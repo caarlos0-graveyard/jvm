016ccc588325b0e0366060476e73985307f2b83f
Carlos A Becker <caarlos0@gmail.com>
2015-01-27 16:49:22 -0200

    Fixes :fire:

--- jvm.plugin.zsh
+++ jvm.plugin.zsh
@@ -1,9 +1,14 @@
 #!/bin/zsh
 
 chpwd() {
-  local current="$(pwd)"
-  local java_version="$(cat pom.xml | grep '<java.version>' | sed 's/.*<java.version>1.\(.*\)<\/java.version>.*/\1/')"
-  export JAVA_HOME="/usr/lib/jvm/java-${java_version}-oracle/"
-  export PATH="$JAVA_HOME/bin:$PATH"
+  if [[ -f pom.xml ]]; then
+    local java_version="$(\
+      cat pom.xml | \
+      grep '<java.version>' | \
+      sed 's/.*<java.version>1.\(.*\)<\/java.version>.*/\1/' \
+    )"
+    export JAVA_HOME="/usr/lib/jvm/java-${java_version}-oracle/"
+    export PATH="$JAVA_HOME/bin:$PATH"
+  fi
 }
 
