cc63ea6530dc4630f812d869a7d2ec9b77f17d59
Carlos A Becker <caarlos0@gmail.com>
2015-01-27 15:08:53 -0200

    First version

---
+++ jvm.plugin.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+chpwd() {
+  local current="$(pwd)"
+  local java_version="$(cat pom.xml | grep '<java.version>' | sed 's/.*<java.version>1.\(.*\)<\/java.version>.*/\1/')"
+  export JAVA_HOME="/usr/lib/jvm/java-${java_version}-oracle/"
+  export PATH="$JAVA_HOME/bin:$PATH"
+}
+
