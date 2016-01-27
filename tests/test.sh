362f6c084a01ef5d144a6a802233c775bb931661
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-27 01:21:48 -0200

    fix

--- tests/test.sh
+++ tests/test.sh
@@ -2,7 +2,7 @@
 source jvm.sh
 jvm local 7
 jvm reload
-if ! [ $(jvm version) -eq 7 ]; then
+if ! [ "$(jvm version)" -eq 7 ]; then
   echo "jvm version should be 7"
 fi
-echo $PATH | grep 1.7
+echo "$PATH" | grep 1.7
