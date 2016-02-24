4600104da20bdfab1607ca6618766786e0172c26
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-02-24 01:07:36 -0300

    some solid tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 set -eo pipefail
-echo 8 > .java-version
+find . -name '.java-version' -delete
+
+ROOT="$(pwd)"
+TESTS="$(dirname "${BASH_SOURCE[0]}")"
+
+assert_java() {
+  version="$1"
+  test "$(jvm version)" = "$version" || exit 1
+  echo "$JAVA_HOME" | grep "$version" || exit 1
+}
+
+test_pom7() {
+  cd "$ROOT/$TESTS"/java7
+  jvm reload
+  assert_java 7
+}
+
+test_pom8() {
+  cd "$ROOT/$TESTS"/java8
+  jvm reload
+  assert_java 8
+}
+
 # shellcheck disable=SC1091
 source jvm.sh
+
+echo "jvm global 8"
+jvm global 8
+assert_java 8
+
+echo "jvm global 7"
 jvm local 7
-jvm reload
-test "$(jvm version)" = "7" || echo "jvm version should be 7"
-echo "$JAVA_HOME" | grep 7
+assert_java 7
+
+echo "cdwd java 7 pom"
+test_pom7
+
+echo "cdwd java 8 pom"
+test_pom8
