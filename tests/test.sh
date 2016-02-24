1b9cd16bdc2e2213b6a83527ed5114ee1fdc586f
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-02-24 09:35:37 -0300

    tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -xeo pipefail
+set -eo pipefail
 ROOT="$(pwd)"
 TESTS="$(dirname "${BASH_SOURCE[0]}")"
 
 assert_java() {
   version="$1"
   test "$(jvm version)" = "$version" || exit 1
-  echo "$JAVA_HOME" | grep "$version" || exit 1
 }
 
 test_pom7() {
@@ -34,14 +33,14 @@ source jvm.sh
 
 echo "jvm global 8"
 jvm global 8
-test "$TRAVIS_OS_NAME" != "osx" && assert_java 8
+assert_java 8
 
 echo "jvm local 7"
 jvm local 7
 assert_java 7
 
 echo "cdwd java 8 pom"
-test "$TRAVIS_OS_NAME" != "osx" && test_pom8
+test_pom8
 
 echo "cdwd java 7 pom"
 test_pom7
